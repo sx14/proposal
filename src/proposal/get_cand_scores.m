54905d30d7ef8623835e8f99933df27b90da4013
sx14 <1059363093@qq.com>
2018-01-23 17:32:07 +0800

    all scoring

--- src/proposal/get_cand_scores.m
+++ src/proposal/get_cand_scores.m
@@ -30,11 +30,10 @@ for f = 1:length(hiers)
     sp_flow_info = sp_flow_info_set{f};
     cands_hf(cands_hf > 0) = cands_hf(cands_hf > 0) - double(leave_sum);
     cand_motion_scores = get_motion_scores(cands_hf, sp_flow_info, cand_info(indexes,:),length(hiers));
-%     cand_appearence_scores = regRF_predict(feats,rf_regressor);
-%     max_appearence_score = max(1,max(cand_appearence_scores));
-%     cand_appearence_scores = cand_appearence_scores / max_appearence_score;
-%     scores(indexes,f) = cand_appearence_scores * 0.7 + cand_motion_scores * 0.3;
-    scores(indexes,f) = cand_motion_scores;
+    cand_appearence_scores = regRF_predict(feats,rf_regressor);
+    max_appearence_score = max(1,max(cand_appearence_scores));
+    cand_appearence_scores = cand_appearence_scores / max_appearence_score;
+    scores(indexes,f) = cand_appearence_scores * 0.7 + cand_motion_scores * 0.3;
 end
 
 scores = sort(scores,2,'descend');
